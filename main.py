c84fec71329cdd802e09e4125ca048041ff58647
swapfile3 <swajeetg768@gmail.com>
2020-11-24 18:03:27 +0530

    Squashed commit of the following:
    commit 6834038f369d6e6097429b640e45293d9aefb372
    Author: swapfile3 <swajeetg768@gmail.com>
    Date:   Tue Nov 24 17:57:46 2020 +0530
    
        Add some lifestyle utills
    
        TODO: Complete age calculator

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
+import lifestyle.general
+
 print('--------------Utility Tools--------------')
 print('---------------by Ankii077---------------')
 print('-----------------------------------------')
+print("type 'help' for all commands, 'exit' to quit application")
 
+try:
+    while True:
+        cmd = input('\033[1;36m$\033[0m ')
+        if len(cmd) == 0:
+            continue
+        if cmd == 'exit':
+            break
+        elif cmd == 'bmi':
+            lifestyle.general.bmi(input('height (in cm): '),
+                                  input('weight (in kg): '), input('age: '))
+        elif cmd == 'age':
+            lifestyle.general.age(input('age (DD-MM-YYYY): '))
+        elif cmd == 'help':
+            print('help.exe stopped working')
+        else:
+            print(repr(cmd), ': command not found', sep='')
+            pass
+except KeyboardInterrupt:
+    print()
